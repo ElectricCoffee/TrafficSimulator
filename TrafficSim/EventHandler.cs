642f83e2b073dcda948925dddab8a12366ff5e31
Luraman <ponifi3d@gmail.com>
2015-04-09 12:36:13 +0200

    Added several features
    Added AddEvent method, TrafficEvent class, and redefined
    TrafficEventList

--- TrafficSim/EventHandler.cs
+++ TrafficSim/EventHandler.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrafficSim
-{
-    class EventHandler
-    {
+namespace TrafficSim {
+    public class EventHandler {
+
+        protected List<TrafficEvent> TrafficEventList = new List<TrafficEvent>();
+
+        protected TimeSpan currentTime;
+
+        public void AddEvent(TrafficCallback callbackMethod, TimeSpan delay) {
+            TrafficEventList.Add(new TrafficEvent(callbackMethod, currentTime + delay));
+            TrafficEventList = TrafficEventList.OrderBy(TE => TE.EventTime).ToList();
+        }
     }
 }
---
+++ TrafficSim/TrafficEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficSim
+{
+    public delegate void TrafficCallback();
+    public class TrafficEvent
+    {
+        public TrafficEvent(TrafficCallback initCallbackMethod, TimeSpan initEventTime)
+        {
+            Callback = initCallbackMethod;
+            EventTime = initEventTime;
+        }
+        public TrafficCallback Callback { get; protected set; }
+        public TimeSpan EventTime { get; protected set; }
+    }
+}
