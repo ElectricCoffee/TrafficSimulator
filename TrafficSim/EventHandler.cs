31f5849a2509a11ddc56991a3913f9575af19c65
Luraman <ponifi3d@gmail.com>
2015-04-13 09:24:22 +0200

    Changed a name and added a description

--- TrafficSim/EventHandler.cs
+++ TrafficSim/EventHandler.cs
@@ -5,15 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TrafficSim {
+
     public class EventHandler {
 
-        protected List<TrafficEvent> TrafficEventList = new List<TrafficEvent>();
+        protected List<TrafficEvent> DescreteEventList = new List<TrafficEvent>();
 
         protected TimeSpan currentTime;
-
+        /// <summary>
+        /// Queues up a method to be called after a certain delay.
+        /// </summary>
+        /// <param name="callbackMethod">Method to be called</param>
+        /// <param name="delay">Delay in seconds</param>
         public void AddEvent(TrafficCallback callbackMethod, TimeSpan delay) {
-            TrafficEventList.Add(new TrafficEvent(callbackMethod, currentTime + delay));
-            TrafficEventList = TrafficEventList.OrderBy(TE => TE.EventTime).ToList();
+            DescreteEventList.Add(new TrafficEvent(callbackMethod, currentTime + delay));
+            DescreteEventList = DescreteEventList.OrderBy(TE => TE.EventTime).ToList();
         }
     }
 }
