2741ef0926385fbd75a1da6692f947e2a4cc0bcf
Luraman <ponifi3d@gmail.com>
2015-04-13 09:26:48 +0200

    Documentation clarity

--- TrafficSim/EventHandler.cs
+++ TrafficSim/EventHandler.cs
@@ -15,7 +15,7 @@ namespace TrafficSim {
         /// Queues up a method to be called after a certain delay.
         /// </summary>
         /// <param name="callbackMethod">Method to be called</param>
-        /// <param name="delay">Delay in seconds</param>
+        /// <param name="delay">Trigger delay</param>
         public void AddEvent(TrafficCallback callbackMethod, TimeSpan delay) {
             DescreteEventList.Add(new TrafficEvent(callbackMethod, currentTime + delay));
             DescreteEventList = DescreteEventList.OrderBy(TE => TE.EventTime).ToList();
