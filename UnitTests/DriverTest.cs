52ebcc3e1f782fa93eb08ded29d470d90ff0d074
Niko Lepka <slench102+git@gmail.com>
2015-04-14 13:00:40 +0200

    Started Driver

---
+++ UnitTests/DriverTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TrafficSim.Entity;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DriverTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
