8bd256202741a3fa9380dd839ad1d14dcda4571a
Niko Lepka <slench102+git@gmail.com>
2015-04-14 13:19:41 +0200

    Added basic fields
    + added basic fields
    + added DriverAggression struct

--- TrafficSim/Entity/Driver.cs
+++ TrafficSim/Entity/Driver.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace TrafficSim.Entity
 {
-    class Driver
+    public class Driver
     {
+        public int VelocityTolerance { get; set; }
+        public int ReactionTime { get; set; }
+        public DriverAggression Aggression { get; set; }
+        public int SafeDistance { get; set; }
+        public int SpeedLimit { get; set; }
+
+        private void SetTargets()
+        {
+
+        }
+
+        private void ReadSign()
+        {
+
+        }
+    }
+
+    public struct DriverAggression
+    {
+        public DriverAggression(int acc, int dec)
+        {
+            Acceleration = acc;
+            Deceleration = dec;
+        }
+
+        public int Acceleration { get; private set; }
+        public int Deceleration { get; private set; }
     }
 }
