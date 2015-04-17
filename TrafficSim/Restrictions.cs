aa881cc5a3d5420baabd1ef3dcc343aca859b60d
monx25 <monx25@hotmail.com>
2015-04-17 12:57:47 +0200

    Tilføjet filer med sign

---
+++ TrafficSim/Restrictions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Restrictions : Sign
+    {
+        string Restriction;
+
+        public override string RoadRestriction()
+        {
+            return Restriction;
+        }
+    }
+}
