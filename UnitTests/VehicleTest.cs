3f2b74ae6809ba7df1b1c1099f7cd727824453aa
mortenm12 <mortenm12@hotmail.com>
2015-04-17 18:16:32 +0200

    Decceleration test works

--- UnitTests/VehicleTest.cs
+++ UnitTests/VehicleTest.cs
@@ -28,13 +28,14 @@ namespace UnitTests
             Car car1 = new Car(50, 50);
             car1.Move(100, 50);
             Assert.AreEqual(car1.Coordinate, new Point(100, 50));
+            Assert.AreEqual(car1.PictureBox.Location, new Point(100, 50));
         }
 
         [TestMethod]
         public void RotationTest()
         {
             Car car1 = new Car(50, 50);
-            car1.Direction = new Point(0, 100);
+            car1.Direction = new Point(-50, 100);
             car1.Move(40, 60);
             //how to get rotation?
 #warning           Assert.AreEqual(, RotateFlipType.Rotate270FlipNone); //how do I get the rotation of the image?
@@ -48,12 +49,20 @@ namespace UnitTests
             car1.Acc = 1;
             car1.Accelerate(2000);
             Assert.AreEqual(car1.Coordinate, new Point(82,50));
+            Assert.AreEqual(car1.PictureBox.Location, new Point(82, 50));
         }
 
         [TestMethod]
         public void DecelerationTest()
         {
-
+            Car car1 = new Car(50, 50);
+            car1.Direction = new Point(50, 0);
+            car1.Decc = 1;
+            car1.Speed = 50;
+            car1.IsBreaking = true;
+            car1.Accelerate(2000);
+            Assert.AreEqual(car1.Coordinate, new Point(818, 50));
+            Assert.AreEqual(car1.PictureBox.Location, new Point(818, 50));
         }
     }
 }
