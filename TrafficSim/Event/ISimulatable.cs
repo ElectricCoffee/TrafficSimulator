91b5d1bb4bacb217bf065143386567edfa195894
Luraman <ponifi3d@gmail.com>
2015-04-21 10:46:09 +0200

    Summary for ISimulatable

--- TrafficSim/Event/ISimulatable.cs
+++ TrafficSim/Event/ISimulatable.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace TrafficSim.Event
 {
+    /// <summary>
+    /// Every object used by TrafficEventHandler should inherit ISimulatable.
+    /// </summary>
     public interface ISimulatable
     {
         TrafficEventHandler eventHandler { get; set; }
