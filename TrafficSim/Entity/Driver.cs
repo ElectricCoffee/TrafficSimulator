d3465e40809c9052dee6b2e01562d1cebfffa368
Niko Lepka <slench102+git@gmail.com>
2015-04-24 15:05:48 +0200

    for some reason didn't get committed before

--- TrafficSim/Entity/Driver.cs
+++ TrafficSim/Entity/Driver.cs
@@ -131,28 +131,4 @@ namespace TrafficSim.Entity
 
         }
     }
-
-    /// <summary>
-    /// Contains the different aggression types the driver can have
-    /// The aggression types define how quickly the driver accelerates and decelerates
-    /// </summary>
-    public class DriverAggression
-    {
-        public DriverAggression() : this(0, 0) { }
-
-        public DriverAggression(int acc, int dec)
-        {
-            Acceleration = acc;
-            Deceleration = dec;
-        }
-
-        /// <summary>
-        /// The driver's willingness to accelerate in %
-        /// </summary>
-        public int Acceleration { get; set; }
-        /// <summary>
-        /// The driver's willingness to decelerate in %
-        /// </summary>
-        public int Deceleration { get; set; }
-    }
 }
