bf116087b8229b36715ec603b8066258ae41bf62
Niko Lepka <slench102+git@gmail.com>
2015-04-27 13:02:35 +0200

    finalised test

--- UnitTests/ErrorLogTest.cs
+++ UnitTests/ErrorLogTest.cs
@@ -7,12 +7,18 @@ namespace UnitTests
     [TestClass]
     public class ErrorLogTest
     {
+        // creates a path to a file called "error-log.txt-test" on the desktop
+        string testErrorPath = Error.DesktopPath + "-test"; 
+
+        /// <summary>
+        /// Removes the file created by the test 
+        /// </summary>
         [TestCleanup]
         public void RemoveAllFiles()
         {
-            if (System.IO.File.Exists(Error.DesktopPath))
+            if (System.IO.File.Exists(testErrorPath))
             {
-                System.IO.File.Delete(Error.DesktopPath); 
+                System.IO.File.Delete(testErrorPath); 
             }
         }
 
@@ -20,21 +26,20 @@ namespace UnitTests
         public void TestStringLogging()
         {
             string expected = "This is a test of an error";
-            Error.Log(expected);
-            string actual = Error.ReadLog();
+            Error.Log(expected, testErrorPath);
+            string actual = Error.ReadLog(testErrorPath);
 
-            Assert.AreEqual(expected, actual);
+            StringAssert.Equals(expected + "\n", actual);
         }
 
         [TestMethod]
         public void TestExceptionLogging()
         {
             var expected = new Exception("This is an exception test");
-            Error.Log(expected);
-            string actual = Error.ReadLog();
-            System.IO.File.Delete(Error.DesktopPath);
+            Error.Log(expected, testErrorPath);
+            string actual = Error.ReadLog(testErrorPath);
 
-            Assert.AreEqual(expected.Message, actual);
+            StringAssert.Equals(expected.Message + "\n", actual);
         }
     }
 }
